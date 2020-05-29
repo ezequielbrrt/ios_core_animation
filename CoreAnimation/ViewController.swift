48e9e05ec9c1beeda2de9a799a771f2e2e261157
Ezequiel Barreto <ezequielbrrt@gmail.com>
2020-05-29 13:00:57 -0500

    Box change color animation

--- CoreAnimation/ViewController.swift
+++ CoreAnimation/ViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let basicView = UIView()
+    
+    var yAnchor:NSLayoutConstraint!
+    var xAnchor:NSLayoutConstraint!
+    var widthAnchor:NSLayoutConstraint!
+    var heightAnchor:NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(basicView)
+        configUI()
+        prepareBasicView()
+    }
+    
+    private func configUI(){
+        self.view.backgroundColor = .white
+    }
+    
+    private func prepareBasicView(){
+        basicView.backgroundColor = .blue
+        basicView.translatesAutoresizingMaskIntoConstraints                      = false
+        basicView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        basicView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        basicView.heightAnchor.constraint(equalToConstant: 200).isActive         = true
+        basicView.widthAnchor.constraint(equalToConstant: 200).isActive          = true
+        
+        animateBox(to: .green)
+        
+    }
+    
+    fileprivate func animateBox(to color: UIColor){
+        UIView.animate(withDuration: 0.3, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.basicView.backgroundColor = color
+        })
     }
 
 
